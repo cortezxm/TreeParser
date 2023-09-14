abbeb86f5d2b76489d0a68930ee2c8e8de24ca13
Ericka <a339451@alumnos.uaslp.mx>
2023-09-14 08:59:12 -0500

    aa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,27 +40,35 @@
             // 
             // PlainText
             // 
-            this.PlainText.Location = new System.Drawing.Point(12, 48);
+            this.PlainText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlainText.Location = new System.Drawing.Point(27, 67);
+            this.PlainText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PlainText.Multiline = true;
             this.PlainText.Name = "PlainText";
-            this.PlainText.Size = new System.Drawing.Size(239, 332);
+            this.PlainText.Size = new System.Drawing.Size(317, 400);
             this.PlainText.TabIndex = 1;
             this.PlainText.UseSystemPasswordChar = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.Size = new System.Drawing.Size(311, 54);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Text View";
+            this.label1.Text = "Ingrese valores raiz, padre \r\ne hijo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Execute
             // 
-            this.Execute.Location = new System.Drawing.Point(176, 415);
+            this.Execute.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Execute.Location = new System.Drawing.Point(195, 484);
+            this.Execute.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Execute.Name = "Execute";
-            this.Execute.Size = new System.Drawing.Size(75, 23);
+            this.Execute.Size = new System.Drawing.Size(117, 36);
             this.Execute.TabIndex = 3;
             this.Execute.Text = "Ejecutar";
             this.Execute.UseVisualStyleBackColor = true;
@@ -68,9 +76,11 @@
             // 
             // Open
             // 
-            this.Open.Location = new System.Drawing.Point(12, 414);
+            this.Open.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Open.Location = new System.Drawing.Point(68, 484);
+            this.Open.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Open.Name = "Open";
-            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.Size = new System.Drawing.Size(100, 36);
             this.Open.TabIndex = 4;
             this.Open.Text = "Abrir";
             this.Open.UseVisualStyleBackColor = true;
@@ -78,26 +88,34 @@
             // 
             // TreeView1
             // 
-            this.TreeView1.Location = new System.Drawing.Point(324, 48);
+            this.TreeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TreeView1.Location = new System.Drawing.Point(432, 59);
+            this.TreeView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TreeView1.Name = "TreeView1";
-            this.TreeView1.Size = new System.Drawing.Size(464, 332);
+            this.TreeView1.Size = new System.Drawing.Size(617, 408);
             this.TreeView1.TabIndex = 5;
             this.TreeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView_AfterSelect);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(321, 22);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(600, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(267, 34);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Tree View";
+            this.label2.Text = "V i s u a l i z a d o r";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Exit
             // 
-            this.Exit.Location = new System.Drawing.Point(712, 415);
+            this.Exit.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(747, 484);
+            this.Exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.Size = new System.Drawing.Size(100, 36);
             this.Exit.TabIndex = 7;
             this.Exit.Text = "Salir";
             this.Exit.UseVisualStyleBackColor = true;
@@ -105,9 +123,11 @@
             // 
             // clear
             // 
-            this.clear.Location = new System.Drawing.Point(324, 415);
+            this.clear.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(617, 484);
+            this.clear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.clear.Name = "clear";
-            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.Size = new System.Drawing.Size(100, 36);
             this.clear.TabIndex = 8;
             this.clear.Text = "Limpiar";
             this.clear.UseVisualStyleBackColor = true;
@@ -115,9 +135,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.clear);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.label2);
@@ -126,8 +146,9 @@
             this.Controls.Add(this.Execute);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PlainText);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TreeParser";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Form1.cs
+++ Form1.cs
@@ -104,5 +104,15 @@ namespace TreeParser
         {
             TreeView1.Nodes.Clear();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
